032b14150866f257a6ff9ba153990b53c6b96c7a
Cody <cydoentis@gmail.com>
2025-06-04 13:14:15 -0400

    update: FileObject.cs now included a StoredName field.

--- CloudCrate.Domain/Entities/FileObject.cs
+++ CloudCrate.Domain/Entities/FileObject.cs
@@ -4,6 +4,7 @@ public class FileObject
 {
     public Guid Id { get; set; }
     public string FileName { get; set; } = null!;
+    public string StoredName { get; set; } = null!;
     public string ContentType { get; set; } = null!;
     public long Size { get; set; }
 
