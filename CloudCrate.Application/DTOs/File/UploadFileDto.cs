c735e97ff9494e337dc746800cc9fff7351010cd
Cody <cydoentis@gmail.com>
2025-06-04 13:13:55 -0400

    Implement: UploadFileDto.cs

---
+++ CloudCrate.Application/DTOs/File/UploadFileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudCrate.Application.DTOs.File;
+
+public class UploadFileDto
+{
+    public Stream FileStream { get; set; } = null!;
+    public string FileName { get; set; } = null!;
+    public string ContentType { get; set; } = "application/octet-stream";
+    public long Size { get; set; }
+}
