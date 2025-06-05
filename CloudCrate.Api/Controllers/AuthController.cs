426af7d618b8f956098910d79db098c1ac40a64e
Cody <cydoentis@gmail.com>
2025-06-04 22:57:45 -0400

    implement: Added AuthController.cs and DTO tro send UserTokenInfo.cs

---
+++ CloudCrate.Api/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using CloudCrate.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CloudCrate.Api.Models;
+using CloudCrate.Application.Common.Interfaces;
+using CloudCrate.Application.DTOs.Auth;
+
+namespace CloudCrate.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IJwtTokenService _jwtTokenService;
+
+    public AuthController(UserManager<ApplicationUser> userManager, IJwtTokenService jwtTokenService)
+    {
+        _userManager = userManager;
+        _jwtTokenService = jwtTokenService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var user = new ApplicationUser { UserName = request.Email, Email = request.Email };
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors.Select(e => e.Description));
+        }
+
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, request.Password))
+        {
+            return Unauthorized("Invalid credentials");
+        }
+
+        var token = _jwtTokenService.GenerateToken(new UserTokenInfo
+        {
+            UserId = user.Id,
+            Email = user.Email!
+        });
+
+        return Ok(new { token });
+    }
+}
---
+++ CloudCrate.Application/DTOs/Auth/UserTokenInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace CloudCrate.Api.Models;
+
+public class UserTokenInfo
+{
+    public string UserId { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
