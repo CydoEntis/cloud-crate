ba570dcaf3493a910ab2339af3acfdb80d5bffd1
Cody <cydoentis@gmail.com>
2025-06-07 10:48:54 -0400

    implement: ApiResponse.cs

---
+++ CloudCrate.Api/Models/ApiResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace CloudCrate.Api.Models;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }  
+    public T? Data { get; set; }
+    public IEnumerable<string>? Errors { get; set; }
+
+    public static ApiResponse<T> SuccessResponse(T data) => new()
+    {
+        Success = true,
+        Data = data,
+        Errors = null
+    };
+
+    public static ApiResponse<T> FailResponse(IEnumerable<string> errors) => new()
+    {
+        Success = false,
+        Data = default,
+        Errors = errors
+    };
+    
+    public static ApiResponse<T> FailResponse(string error) => FailResponse([error]);
+}
