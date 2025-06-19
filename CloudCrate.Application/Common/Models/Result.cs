9155f49b5b0ccf720d7db9c0e3206c242e270d7c
Cody <cydoentis@gmail.com>
2025-06-19 10:18:12 -0400

    refactor: Result class now handles errors using a dictionary instead of an array of strings.

--- CloudCrate.Application/Common/Models/Result.cs
+++ CloudCrate.Application/Common/Models/Result.cs
@@ -3,12 +3,15 @@
 public class Result
 {
     public bool Succeeded { get; set; }
-    public List<string> Errors { get; set; } = new();
+    public Dictionary<string, string> Errors { get; set; } = new();
 
     public static Result Success() => new() { Succeeded = true };
 
-    public static Result Failure(params string[] errors) =>
-        new() { Succeeded = false, Errors = errors.ToList() };
+    public static Result Failure(string key, string message) =>
+        new() { Succeeded = false, Errors = new Dictionary<string, string> { [key] = message } };
+
+    public static Result Failure(Dictionary<string, string> errors) =>
+        new() { Succeeded = false, Errors = errors };
 }
 
 public class Result<T> : Result
@@ -18,6 +21,9 @@ public class Result<T> : Result
     public static Result<T> Success(T data) =>
         new Result<T> { Succeeded = true, Data = data };
 
-    public new static Result<T> Failure(params string[] errors) =>
-        new Result<T> { Succeeded = false, Errors = errors.ToList() };
+    public new static Result<T> Failure(Dictionary<string, string> errors) =>
+        new Result<T> { Succeeded = false, Errors = errors };
+
+    public new static Result<T> Failure(string key, string message) =>
+        new Result<T> { Succeeded = false, Errors = new Dictionary<string, string> { [key] = message } };
 }
