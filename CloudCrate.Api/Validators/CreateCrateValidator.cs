8b60375d42160c41d7b820d09a0c1c6d9beb5bb4
Cody <cydoentis@gmail.com>
2025-06-21 15:53:24 -0400

    implement: CreateCrateRequest.cs and CreateCrateValidator.cs

---
+++ CloudCrate.Api/Validators/CreateCrateValidator.cs
@@ -0,0 +1,14 @@
+﻿using CloudCrate.Api.Requests.Crate;
+using FluentValidation;
+
+namespace CloudCrate.Api.Validators;
+
+public class CreateCrateRequestValidator : AbstractValidator<CreateCrateRequest>
+{
+    public CreateCrateRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
