d1bf4c0cbcb5bef26f539c06512202ca555ccf1d
Cody <cydoentis@gmail.com>
2025-06-23 21:44:57 -0400

    implement: validation logic to check for a valid color.

--- CloudCrate.Api/Validators/CreateCrateValidator.cs
+++ CloudCrate.Api/Validators/CreateCrateValidator.cs
@@ -11,5 +11,15 @@ public class CreateCrateRequestValidator : AbstractValidator<CreateCrateRequest>
             .NotEmpty()
             .MaximumLength(100)
             .Matches("^[a-zA-Z0-9 _-]*$").WithMessage("Crate name contains invalid characters.");
+
+        RuleFor(x => x.Color)
+            .NotEmpty().WithMessage("Color is required.")
+            .Must(IsValidColor).WithMessage("Color is invalid.");
+    }
+
+    private bool IsValidColor(string color)
+    {
+        if (string.IsNullOrEmpty(color)) return false;
+        return System.Text.RegularExpressions.Regex.IsMatch(color, "^#([0-9A-Fa-f]{6})$");
     }
 }
