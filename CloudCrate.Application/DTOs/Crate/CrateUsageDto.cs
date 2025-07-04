93c7432a8e34a6230cee8b5489e23e5e8d8b1ff5
Cody <cydoentis@gmail.com>
2025-07-04 12:50:31 -0400

    implement: CrateUsageDto.cs

---
+++ CloudCrate.Application/DTOs/Crate/CrateUsageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudCrate.Application.DTOs.Crate;
+
+public class CrateUsageDto
+{
+    public int TotalUsed { get; set; }
+    public int StorageLimit { get; set; }
+    public int Remaining => StorageLimit - TotalUsed;
+    public Dictionary<string, int> BreakdownByType { get; set; } = new();
+}
