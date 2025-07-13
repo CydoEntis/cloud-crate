303df56b188afa7fc8ecbc1398c373f350ec6ce3
Cody <cydoentis@gmail.com>
2025-07-13 15:19:57 -0400

    implement: Updated storage settings to include minio options.

--- CloudCrate.Application/Common/Settings/StorageSettings.cs
+++ CloudCrate.Application/Common/Settings/StorageSettings.cs
@@ -3,4 +3,7 @@
 public class StorageSettings
 {
     public string RootPath { get; set; } = string.Empty;
+    public string Endpoint { get; set; } = string.Empty;
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
 }
