e7ce0c33e44044bcaf64c02e7fd2ccae3f514cc4
Cody <cydoentis@gmail.com>
2025-07-14 10:49:42 -0400

    refactor: UploadFileRequestValidator.cs now checks against the list of allowed file extensions.

--- CloudCrate.Api/Validators/UploadFileRequestValidator.cs
+++ CloudCrate.Api/Validators/UploadFileRequestValidator.cs
@@ -1,4 +1,5 @@
-﻿using CloudCrate.Api.Models;
+﻿using CloudCrate.Api.Common.Extensions;
+using CloudCrate.Api.Models;
 using CloudCrate.Api.Requests.File;
 using FluentValidation;
 
@@ -6,15 +7,15 @@ namespace CloudCrate.Api.Validators;
 
 public class UploadFileRequestValidator : AbstractValidator<UploadFileRequest>
 {
-    private readonly string[] _permittedExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff" };
-
     public UploadFileRequestValidator()
     {
-        RuleFor(x => x.File).NotNull().WithMessage("A file must be provided.");
-
         RuleFor(x => x.File)
+            .NotNull().WithMessage("A file must be provided.")
             .Must(file =>
-                file != null && _permittedExtensions.Contains(Path.GetExtension(file.FileName).ToLowerInvariant()))
-            .WithMessage("Invalid file type. Only image files are allowed.");
+            {
+                var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+                return AllowedFileExtensions.Extensions.Contains(ext);
+            })
+            .WithMessage("Invalid file type. Only allowed file types can be uploaded.");
     }
 }
