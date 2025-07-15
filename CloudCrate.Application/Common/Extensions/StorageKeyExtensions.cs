534616371ce80b76da7d201778b4c34cbb9383d6
Cody <cydoentis@gmail.com>
2025-07-15 10:51:04 -0400

    implement: Helper extension that can get minio object keys.

---
+++ CloudCrate.Application/Common/Extensions/StorageKeyExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CloudCrate.Application.Common.Extensions
+{
+    public static class StorageKeyExtensions
+    {
+        public static string GetObjectKey(this string userId, Guid crateId, Guid? folderId, string fileName)
+        {
+            var parts = new List<string> { userId, crateId.ToString() };
+            if (folderId.HasValue)
+                parts.Add(folderId.Value.ToString());
+            parts.Add(fileName);
+            return string.Join("/", parts);
+        }
+    }
+}
