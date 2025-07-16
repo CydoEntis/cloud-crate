2664a212af2e0bc6a2774dbef3253273ce0d3cff
Cody <cydoentis@gmail.com>
2025-07-16 09:00:20 -0400

    implement: CratePermission.cs and CrateRole.cs to add RBAC

---
+++ CloudCrate.Domain/Entities/CratePermission.cs
@@ -0,0 +1,21 @@
+﻿using CloudCrate.Domain.Enums;
+
+namespace CloudCrate.Domain.Entities;
+
+public class CratePermission
+{
+    public Guid Id { get; set; }
+
+    public Guid CrateId { get; set; }
+    public Crate Crate { get; set; }
+
+    public string UserId { get; set; }
+
+    public CrateRole Role { get; set; }
+
+    public bool? CanUpload { get; set; }
+    public bool? CanDownload { get; set; }
+    public bool? CanDelete { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
---
+++ CloudCrate.Domain/Enums/CrateRole.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudCrate.Domain.Enums;
+
+public enum CrateRole
+{
+    Owner,      // Full control, including deleting the crate
+    Editor,     // Can upload/download/delete files
+    Viewer      // Read-only access
+}
