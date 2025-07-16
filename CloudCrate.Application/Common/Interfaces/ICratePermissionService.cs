2a4905ca221066b8a79efeb2c28676c041911803
Cody <cydoentis@gmail.com>
2025-07-16 09:01:03 -0400

    implement: CratePermissionsService.cs to get what actions a user can perform

---
+++ CloudCrate.Application/Common/Interfaces/ICratePermissionService.cs
@@ -0,0 +1,12 @@
+﻿using CloudCrate.Domain.Entities;
+
+namespace CloudCrate.Application.Common.Interfaces;
+
+public interface ICratePermissionService
+{
+    Task<CratePermission> GetUserPermissionsAsync(Guid crateId, string userId);
+    Task<bool> CanUserUploadAsync(Guid crateId, string userId);
+    Task<bool> CanUserDownloadAsync(Guid crateId, string userId);
+    Task<bool> CanUserDeleteFileAsync(Guid crateId, string userId);
+    Task<bool> IsOwnerAsync(Guid crateId, string userId);
+}
