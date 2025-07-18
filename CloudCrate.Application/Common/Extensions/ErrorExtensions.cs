c2ca2bcf1d732c15f2167f6fa464db8be5e68a9a
Cody <cydoentis@gmail.com>
2025-07-18 13:15:50 -0400

    implement: Added a ErrorExtensions.cs class you can optionally override an error message of a specific error.

---
+++ CloudCrate.Application/Common/Extensions/ErrorExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CloudCrate.Application.Common.Errors;
+
+namespace CloudCrate.Application.Common.Extensions;
+
+public static class ErrorExtensions
+{
+    public static Error WithMessage(this Error error, string message)
+    {
+        return new Error(error.Code, message, error.StatusCode);
+    }
+}
