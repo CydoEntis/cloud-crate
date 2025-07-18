b194de7184e3434bdc72c3e7cba7d93d8c5f37f4
Cody <cydoentis@gmail.com>
2025-07-18 19:37:06 -0400

    implement: ResultExtensions.cs so i can convert results to IActionResults.

---
+++ CloudCrate.Api/Common/Extensions/ResultExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CloudCrate.Api.Models;
+using CloudCrate.Application.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCrate.Api.Common.Extensions;
+
+public static class ResultExtensions
+{
+    public static IActionResult ToActionResult<T>(this Result<T> result)
+    {
+        if (result.Succeeded)
+            return new OkObjectResult(ApiResponse<T>.Success(result.Value!));
+
+        return ApiResponseHelper.FromErrors<T>(result.Errors);
+    }
+
+    public static IActionResult ToActionResult(this Result result)
+    {
+        if (result.Succeeded)
+            return new OkObjectResult(ApiResponse<string>.SuccessMessage());
+
+        return ApiResponseHelper.FromErrors<string>(result.Errors);
+    }
+}
