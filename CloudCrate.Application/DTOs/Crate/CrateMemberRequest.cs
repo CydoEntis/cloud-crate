d4728bfa5d44f3484471d9d7f1b47049a826b03f
Cody <cydoentis@gmail.com>
2025-07-19 13:37:47 -0400

    implement: CrateMemberRequest.cs to consolidate the data needed to fetch members.

---
+++ CloudCrate.Application/DTOs/Crate/CrateMemberRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudCrate.Application.DTOs.Crate;
+
+public class CrateMemberRequest
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? Email { get; set; }
+}
