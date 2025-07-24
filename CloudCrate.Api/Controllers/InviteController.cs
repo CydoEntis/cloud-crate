874d7d6e68c21992a98ce66e51f89795a157103c
Cody <cydoentis@gmail.com>
2025-07-24 11:39:04 -0400

    refactor: Renamed InvitesController.cs to InviteController, as it only controls one invite.

--- CloudCrate.Api/Controllers/InviteController.cs
+++ CloudCrate.Api/Controllers/InviteController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using CloudCrate.Api.Common.Extensions;
 using CloudCrate.Api.Models;
+using CloudCrate.Application.DTOs.Invite.Request;
 using CloudCrate.Application.Interfaces.Crate;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,17 +9,38 @@ using Microsoft.AspNetCore.Mvc;
 namespace CloudCrate.Api.Controllers
 {
     [ApiController]
-    [Route("api/invites")]
-    public class InvitesController : ControllerBase
+    [Route("api/invite")]
+    public class InviteController : ControllerBase
     {
         private readonly ICrateInviteService _inviteService;
         private readonly ICrateUserRoleService _roleService;
 
-        public InvitesController(ICrateInviteService inviteService, ICrateUserRoleService roleService)
+        public InviteController(ICrateInviteService inviteService, ICrateUserRoleService roleService)
         {
             _inviteService = inviteService;
             _roleService = roleService;
         }
+
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> SendInvite(
+            [FromBody] CrateInviteRequest request)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrWhiteSpace(userId))
+                return Unauthorized(ApiResponse<object>.Unauthorized("You must be logged in to send invites."));
+
+            var result = await _inviteService.CreateInviteAsync(
+                request.CrateId,
+                request.Email,
+                userId,
+                request.Role,
+                request.ExpiresAt);
+
+            return result.ToActionResult(this, successMessage: "Invite sent");
+        }
+
 
         [HttpGet("token/{token}")]
         [AllowAnonymous]
