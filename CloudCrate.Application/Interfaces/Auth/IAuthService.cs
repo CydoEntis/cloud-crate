b409b9f1e216bcf372fefe95c930e1f17f74ab47
Cody <cydoentis@gmail.com>
2025-07-26 14:03:33 -0400

    refactor: Removed unused methods from AuthService.cs and IAuthService.cs

--- CloudCrate.Application/Interfaces/Auth/IAuthService.cs
+++ CloudCrate.Application/Interfaces/Auth/IAuthService.cs
@@ -7,5 +7,4 @@ public interface IAuthService
 {
     Task<Result<UserResponse>> RegisterAsync(string email, string password);
     Task<Result<string>> LoginAsync(string email, string password);
-    Task<Result<UserResponse>> GetUserByIdAsync(string userId);
 }
