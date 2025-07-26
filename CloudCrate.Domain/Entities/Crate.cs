fdaaad4d52efa93d57ec7ff60f734be97d056fa8
Cody <cydoentis@gmail.com>
2025-07-26 11:48:26 -0400

    refactor: Updated how Crate Crate works now instead of assigning the user id directly to the crate, when a crate is created a Crate Member is added with an Owner Role to a separate table

--- CloudCrate.Domain/Entities/Crate.cs
+++ CloudCrate.Domain/Entities/Crate.cs
@@ -1,16 +1,17 @@
-﻿namespace CloudCrate.Domain.Entities;
+﻿using CloudCrate.Domain.Enums;
+
+namespace CloudCrate.Domain.Entities;
 
 public class Crate
 {
     public Guid Id { get; private set; }
     public string Name { get; private set; }
-    public string UserId { get; private set; }
     public string Color { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime UpdatedAt { get; private set; }
     public ICollection<Folder> Folders { get; set; }
     public ICollection<FileObject> Files { get; set; }
-    public ICollection<CrateUserRole> Members { get; set; }
+    public ICollection<CrateMember> Members { get; set; }
 
     protected Crate()
     {
@@ -18,18 +19,21 @@ public class Crate
 
     public static Crate Create(string name, string userId, string? color = null)
     {
-        return new Crate
+        var crate = new Crate
         {
             Id = Guid.NewGuid(),
             Name = name,
-            UserId = userId,
             Color = color ?? "#4B9CED",
             CreatedAt = DateTime.UtcNow,
             UpdatedAt = DateTime.UtcNow,
             Folders = new List<Folder>(),
             Files = new List<FileObject>(),
-            Members = new List<CrateUserRole>()
+            Members = new List<CrateMember>()
         };
+
+        crate.Members.Add(CrateMember.Create(crate.Id, userId, CrateRole.Owner));
+
+        return crate;
     }
 
     public void Rename(string newName)
