e4e0890920df50781fd7a66220d4c73e2c704a4a
Cody <cydoentis@gmail.com>
2025-07-27 14:35:23 -0400

    implement: Added extra properties to CrateResponse.

--- CloudCrate.Application/DTOs/Crate/Response/CrateResponse.cs
+++ CloudCrate.Application/DTOs/Crate/Response/CrateResponse.cs
@@ -1,8 +1,13 @@
-﻿namespace CloudCrate.Application.DTOs.Crate.Response;
+﻿using CloudCrate.Domain.Enums;
+
+namespace CloudCrate.Application.DTOs.Crate.Response;
 
 public class CrateResponse
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public string Color { get; set; } = null!;
+    public string Owner { get; set; } = null!;
+    public CrateRole Role { get; set; } 
+    public List<CrateMemberResponse> Members { get; set; }
 }
