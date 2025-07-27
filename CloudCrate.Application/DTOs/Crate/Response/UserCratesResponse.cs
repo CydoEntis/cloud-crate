ad9bc5b1f0ad8091ac6933c4f0a0908229c27acc
Cody <cydoentis@gmail.com>
2025-07-27 12:12:18 -0400

    implement: UserCratesResponse.cs so joined and owned crates can be returned to the client.

---
+++ CloudCrate.Application/DTOs/Crate/Response/UserCratesResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CloudCrate.Application.DTOs.Crate.Response;
+
+public class UserCratesResponse
+{
+    public List<CrateResponse> Owned { get; set; } = new();
+    public List<CrateResponse> Joined { get; set; } = new();
+}
