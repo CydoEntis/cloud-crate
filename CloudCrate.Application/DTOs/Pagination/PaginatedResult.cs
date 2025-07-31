a625f78d813214c94c8d3453c9430938aa816983
Cody <cydoentis@gmail.com>
2025-07-31 11:30:57 -0400

    implement: PaginatedResult.cs DTO to send back paginated results to the client.

---
+++ CloudCrate.Application/DTOs/Pagination/PaginatedResult.cs
@@ -0,0 +1,20 @@
+﻿namespace CloudCrate.Application.DTOs.Pagination;
+
+public class PaginatedResult<T>
+{
+    public List<T> Items { get; set; } = [];
+    public int TotalCount { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+
+    public static PaginatedResult<T> Create(List<T> items, int totalCount, int page, int pageSize)
+    {
+        return new PaginatedResult<T>
+        {
+            Items = items,
+            TotalCount = totalCount,
+            Page = page,
+            PageSize = pageSize
+        };
+    }
+}
