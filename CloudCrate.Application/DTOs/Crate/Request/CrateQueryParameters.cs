586a16e73aa43a025e6a8cc60bae6f9b4ec0969a
Cody <cydoentis@gmail.com>
2025-08-06 11:51:38 -0400

    implement; OwnerOnly CrateQueryParameters.cs so i can filter crates by owner/member

--- CloudCrate.Application/DTOs/Crate/Request/CrateQueryParameters.cs
+++ CloudCrate.Application/DTOs/Crate/Request/CrateQueryParameters.cs
@@ -8,7 +8,7 @@ public class CrateQueryParameters
     public CrateSortBy? SortBy { get; set; } = null;
     public OrderBy OrderBy { get; set; } = OrderBy.Desc;
     public string? SearchTerm { get; set; } = null;
-    
+    public bool? OwnerOnly { get; set; } = null;
     public int Page { get; set; } = 1;
     public int PageSize { get; set; } = 10;
 }
