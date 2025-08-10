26f0588684cddfdff42745262285d195341848f6
Cody <cydoentis@gmail.com>
2025-08-10 13:41:03 -0400

    implement: FileUrl to FileObjectResponse.cs for use on the client.

--- CloudCrate.Application/DTOs/File/Response/FileObjectResponse.cs
+++ CloudCrate.Application/DTOs/File/Response/FileObjectResponse.cs
@@ -6,6 +6,7 @@ public class FileObjectResponse
     public string Name { get; set; }
     public long SizeInBytes { get; set; }
     public string MimeType { get; set; }
+    public string FileUrl { get; set; }
     public Guid CrateId { get; set; }
     public Guid? FolderId { get; set; }
     public Guid? CategoryId { get; set; }
