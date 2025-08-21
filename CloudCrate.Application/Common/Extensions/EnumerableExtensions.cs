d57cf53c98237961f269cea35c3f7cfc36c73332
Cody <cydoentis@gmail.com>
2025-08-21 12:43:34 -0400

    implement: Paginate extension for enumerable objects in EnumerableExtensions.cs

---
+++ CloudCrate.Application/Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CloudCrate.Application.DTOs.Pagination;
+
+namespace CloudCrate.Application.Common.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static PaginatedResult<T> Paginate<T>(
+        this IEnumerable<T> source,
+        int page,
+        int pageSize)
+    {
+        if (page < 1) page = 1;
+        if (pageSize < 1) pageSize = 20;
+
+        var totalCount = source.Count();
+        var items = source
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        return PaginatedResult<T>.Create(items, totalCount, page, pageSize);
+    }
+}
