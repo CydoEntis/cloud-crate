855c142efc045ee3d4f95a4e77abeb0704b5926d
Cody <cydoentis@gmail.com>
2025-08-22 13:03:52 -0400

    refactor: Removed RenameFolderRequest.cs and added a more robust UpdateFolderRequest.cs

--- CloudCrate.Application/DTOs/Folder/Request/RenameFolderRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CloudCrate.Application.DTOs.Folder.Request;
-
-public class RenameFolderRequest
-{
-    public Guid FolderId { get; set; }
-    public string NewName { get; set; } = null!;
-}
---
+++ CloudCrate.Application/DTOs/Folder/Request/UpdateFolderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudCrate.Application.DTOs.Folder.Request;
+
+public class UpdateFolderRequest
+{
+    public Guid FolderId { get; set; }
+    public string? NewName { get; set; }
+    public string? NewColor { get; set; }
+}
