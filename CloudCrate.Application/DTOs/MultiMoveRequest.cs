e5839aaec3649d8bb8eeb2f11c6b354583aea37c
Cody <cydoentis@gmail.com>
2025-08-23 13:36:29 -0400

    implement: DTO's for MultiRestoreRequest.cs and MultiMoveRequest.cs

---
+++ CloudCrate.Application/DTOs/MultiMoveRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudCrate.Application.DTOs;
+
+public class MultipleMoveRequest
+{
+    public Guid CrateId { get; set; }
+    public List<Guid> FolderIds { get; set; } = new();
+
+    public List<Guid> FileIds { get; set; } = new();
+
+    // null or Guid.Empty => move to root
+    public Guid? NewParentId { get; set; }
+}
---
+++ CloudCrate.Application/DTOs/MultiRestoreRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudCrate.Application.DTOs;
+
+public class MultipleRestoreRequest
+{
+    public Guid CrateId { get; set; }
+    public List<Guid> FolderIds { get; set; } = new();
+    public List<Guid> FileIds { get; set; } = new();
+}
