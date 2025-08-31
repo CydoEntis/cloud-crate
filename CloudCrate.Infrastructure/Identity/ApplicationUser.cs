8650c7dbe3eeb10bb1521b991df8978ca2d9dc2a
Cody <cydoentis@gmail.com>
2025-08-31 18:03:46 -0400

    refactor: Updated ApplicationUser.cs to now set hard limits on DisplayName and ProfilePictureUrl and added two new fields CreatedAt and UpdatedAt, as well as a Factory Method to map the IdentityUser to a Domain Usable CrateUser.

--- CloudCrate.Infrastructure/Identity/ApplicationUser.cs
+++ CloudCrate.Infrastructure/Identity/ApplicationUser.cs
@@ -1,4 +1,6 @@
-﻿using CloudCrate.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using CloudCrate.Domain.Entities;
+using CloudCrate.Domain.Enums;
 using Microsoft.AspNetCore.Identity;
 
 namespace CloudCrate.Infrastructure.Identity;
@@ -6,6 +8,23 @@ namespace CloudCrate.Infrastructure.Identity;
 public class ApplicationUser : IdentityUser
 {
     public SubscriptionPlan Plan { get; set; }
+    [MaxLength(100)] 
     public string DisplayName { get; set; } = string.Empty;
+
+    [MaxLength(500)]
     public string ProfilePictureUrl { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdadatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public CrateUser ToDomainUser() =>
+        CrateUser.Create(
+            Id,
+            DisplayName,
+            Email,
+            ProfilePictureUrl,
+            Plan,
+            CreatedAt,
+            UpdadatedAt
+        );
 }
