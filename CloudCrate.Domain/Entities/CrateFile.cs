5cbd2bea07fb95bd6df548c6e57bbaa81402c64b
Cody <cydoentis@gmail.com>
2025-08-31 18:01:40 -0400

    implement: Crate specific Domain types CrateFolder, CrateUser, CrateFile,

---
+++ CloudCrate.Domain/Entities/CrateFile.cs
@@ -0,0 +1,49 @@
+﻿namespace CloudCrate.Domain.Entities;
+
+public class CrateFile
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public long SizeInBytes { get; set; } = 0;
+    public string MimeType { get; set; } = string.Empty;
+    public string ObjectKey { get; set; } = string.Empty;
+
+    public Guid CrateId { get; set; }
+    public Crate Crate { get; set; }
+
+    public Guid? CrateFolderId { get; set; }
+    public CrateFolder? CrateFolder { get; set; }
+
+    public required string UploaderId { get; set; }
+    public CrateUser Uploader { get; set; } = null!; // navigation property
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
+
+    public static CrateFile Create(
+        string name,
+        long sizeInBytes,
+        string mimeType,
+        Guid crateId,
+        string uploaderId,
+        Guid? crateFolderId = null
+    )
+    {
+        return new CrateFile
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            SizeInBytes = sizeInBytes,
+            MimeType = mimeType,
+            CrateId = crateId,
+            CrateFolderId = crateFolderId,
+            UploaderId = uploaderId,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow,
+            IsDeleted = false
+        };
+    }
+}
---
+++ CloudCrate.Domain/Entities/CrateFolder.cs
@@ -0,0 +1,47 @@
+﻿namespace CloudCrate.Domain.Entities;
+
+public class CrateFolder
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; } = "#EAAC00";
+
+    public Guid? ParentFolderId { get; set; }
+    public Folder? ParentFolder { get; set; }
+
+    public Guid CrateId { get; set; }
+    public Crate Crate { get; set; }
+
+    public ICollection<FileObject> Files { get; set; } = new List<FileObject>();
+    public ICollection<Folder> Subfolders { get; set; } = new List<Folder>();
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public string UserId { get; set; }
+    public CrateUser User { get; set; }
+
+    public bool IsDeleted { get; set; } = false;
+
+    public static CrateFolder Create(
+        string name,
+        Guid crateId,
+        Guid? parentFolderId,
+        string? color,
+        string userId
+    )
+    {
+        return new CrateFolder
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            CrateId = crateId,
+            ParentFolderId = parentFolderId,
+            Color = color ?? "#EAAC00",
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow,
+            UserId = userId,
+            IsDeleted = false
+        };
+    }
+}
