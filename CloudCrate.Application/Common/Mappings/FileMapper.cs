eabb4e30656ba2b1e1cf84b955cdd56669f50304
Cody <cydoentis@gmail.com>
2025-08-31 18:02:38 -0400

    implement: FileMapper.cs and UserMapper.cs so mapping logic is kept separate from the associated Service Layer.

---
+++ CloudCrate.Application/Common/Mappings/FileMapper.cs
@@ -0,0 +1,41 @@
+﻿using CloudCrate.Application.DTOs.File;
+using CloudCrate.Application.DTOs.User;
+using CloudCrate.Application.DTOs.User.Mappers;
+using CloudCrate.Application.DTOs.User.Response;
+using CloudCrate.Domain.Entities;
+
+namespace CloudCrate.Application.Common.Mappings;
+
+public static class FileMapper
+{
+    public static SoftDeletedFile MapToSoftDeletedFile(CrateFile file)
+    {
+        return new SoftDeletedFile
+        {
+            Id = file.Id,
+            Name = file.Name,
+            SizeInBytes = file.SizeInBytes,
+            MimeType = file.MimeType,
+            IsDeleted = file.IsDeleted,
+            DeletedAt = file.DeletedAt
+        };
+    }
+
+    public static CrateFileResponse ToCrateFileResponse(CrateFile file, string fileUrl)
+    {
+        return new CrateFileResponse
+        {
+            Id =  file.Id,
+            Name = file.Name,
+            SizeInBytes = file.SizeInBytes,
+            MimeType = file.MimeType,
+            FileUrl = fileUrl,
+            IsDeleted = file.IsDeleted,
+            CrateId = file.CrateId,
+            FolderId = file.CrateFolderId ?? null,
+            FolderName = file.CrateFolder?.Name,
+            Uploader = UserMapper.ToUploader(file.Uploader),
+            CreatedAt = file.CreatedAt,
+        };
+    }
+}
---
+++ CloudCrate.Application/DTOs/User/Mappers/UserMapper.cs
@@ -0,0 +1,33 @@
+﻿using CloudCrate.Application.DTOs.User.Response;
+using CloudCrate.Domain.Entities;
+
+namespace CloudCrate.Application.DTOs.User.Mappers;
+
+public static class UserMapper
+{
+    public static Uploader ToUploader(UserResponse user) =>
+        new Uploader
+        {
+            UserId = user.Id,
+            DisplayName = user.DisplayName,
+            Email = user.Email,
+            ProfilePictureUrl = user.ProfilePictureUrl
+        };
+    
+    public static Uploader ToUploader(CrateUser user) =>
+        new Uploader
+        {
+            UserId = user.Id,
+            DisplayName = user.DisplayName,
+            Email = user.Email,
+            ProfilePictureUrl = user.ProfilePictureUrl
+        };
+
+    public static UserResponse ToUserResponse(CrateUser user) => new UserResponse
+    {
+        Id = user.Id,
+        Email = user.Email,
+        DisplayName = user.DisplayName,
+        ProfilePictureUrl = user.ProfilePictureUrl
+    };
+}
