cc085a737e584b123e616cbbee991c086022973b
Cody <cydoentis@gmail.com>
2025-09-04 11:28:24 -0400

    refactor: Moved all reused controller logic into a base controller.

---
+++ CloudCrate.Api/Controllers/BaseController.cs
@@ -0,0 +1,48 @@
+﻿using CloudCrate.Api.Models;
+using CloudCrate.Application.Common.Errors;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    protected new IActionResult Response<T>(ApiResponse<T> response) =>
+        StatusCode(response.StatusCode, response);
+    
+    protected new IActionResult Response(ApiResponse response) =>
+        StatusCode(response.StatusCode, response);
+
+    protected string? UserId => User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+    protected IActionResult? EnsureUserAuthenticated()
+    {
+        if (string.IsNullOrWhiteSpace(UserId))
+        {
+            var response = new ApiResponse<string>(
+                isSuccess: false,
+                value: null,
+                message: "User is not authenticated",
+                statusCode: 401
+            );
+            return Response(response);
+        }
+
+        return null;
+    }
+
+    protected IActionResult? EnsureRouteIdMatches(Guid routeId, Guid bodyId, string name)
+    {
+        if (routeId != bodyId)
+        {
+            var response = new ApiResponse<string>(
+                isSuccess: false,
+                value: null,
+                message: $"{name} ID in route and request body do not match",
+                statusCode: 400
+            );
+            return Response(response);
+        }
+
+        return null;
+    }
+}
