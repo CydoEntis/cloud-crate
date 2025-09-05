2cf421608d73be4e7cadbb683909ddfbcdfbcd18
Cody <cydoentis@gmail.com>
2025-09-05 17:48:11 -0400

    implement: CrateMapper.cs Crate -> CrateResponse

---
+++ CloudCrate.Application/Mappers/CrateMapper.cs
@@ -0,0 +1,32 @@
+﻿using CloudCrate.Application.DTOs.Crate.Response;
+using CloudCrate.Domain.Enums;
+
+namespace CloudCrate.Application.Mappers;
+
+public static class CrateMapper
+{
+    public static CrateResponse ToCrateResponse(this Domain.Entities.Crate crate, string userId)
+    {
+        var owner = crate.Members.FirstOrDefault(m => m.Role == CrateRole.Owner);
+
+        var joined = crate.Members.FirstOrDefault(m => m.UserId == userId);
+
+        return new CrateResponse
+        {
+            Id = crate.Id,
+            Name = crate.Name,
+            Color = crate.Color,
+            Owner = owner is null
+                ? null!
+                : new CrateMemberResponse
+                {
+                    UserId = owner.UserId,
+                    JoinedAt = owner.JoinedAt,
+                    Role = CrateRole.Owner
+                },
+            UsedStorageBytes = crate.UsedStorage.Bytes,
+            TotalStorageBytes = crate.AllocatedStorage.Bytes,
+            JoinedAt = joined?.JoinedAt ?? DateTime.MinValue
+        };
+    }
+}
