6a70a699f8b88455288365669379ac8652092848
Cody <cydoentis@gmail.com>
2025-09-05 11:29:31 -0400

    implement: StorageSize value object.

---
+++ CloudCrate.Domain/ValueObjects/StorageSize.cs
@@ -0,0 +1,17 @@
+﻿namespace CloudCrate.Domain.ValueObjects;
+
+public class StorageSize
+{
+    public long Bytes { get; }
+
+    private StorageSize(long bytes) => Bytes = bytes;
+
+    public static StorageSize FromGigabytes(long gb)
+        => new StorageSize(gb * 1024L * 1024 * 1024);
+
+    public static StorageSize FromBytes(long bytes)
+        => new StorageSize(bytes);
+
+    public double ToGigabytes()
+        => Bytes / 1024d / 1024d / 1024d;
+}
