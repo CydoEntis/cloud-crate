00d8f351cfe4662cf952878bc254cad0af146c0e
Cody <cydoentis@gmail.com>
2025-09-09 21:15:04 -0400

    implement: Added a BatchDeleteService.cs to handle all batch delete functionality

---
+++ CloudCrate.Application/Interfaces/IBatchDeleteService.cs
@@ -0,0 +1,11 @@
+﻿using CloudCrate.Application.Models;
+
+
+namespace CloudCrate.Application.Interfaces;
+
+public interface IBatchDeleteService
+{
+    Task<Result> DeleteFilesAsync(IEnumerable<Guid> fileIds);
+    Task<Result> DeleteFoldersAsync(IEnumerable<Guid> folderIds);
+    Task<Result> DeleteCratesAsync(IEnumerable<Guid> crateIds);
+}
