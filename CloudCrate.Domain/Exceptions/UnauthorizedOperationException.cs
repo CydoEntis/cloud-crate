7517b6ad7bbe4863d536e76c5ba21ad78181aef6
Cody <cydoentis@gmail.com>
2025-09-13 18:03:47 -0400

    implement: Added UnauthorizedOperationException.cs

---
+++ CloudCrate.Domain/Exceptions/UnauthorizedOperationException.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudCrate.Domain.Exceptions;
+
+public class UnauthorizedOperationException : DomainValidationException
+{
+    public UnauthorizedOperationException(string message) : base(message)
+    {
+    }
+}
