68c2f18ac6b35537cfeefd169adfc8c36163506b
Cody <cydoentis@gmail.com>
2025-09-15 11:44:21 -0400

    implement: CrateFolderMapper to help moving Domain Objects to Response objects.

--- CloudCrate.Application/Mappers/CrateFolderMapper.cs
+++ CloudCrate.Application/Mappers/CrateFolderMapper.cs
@@ -5,15 +5,35 @@ namespace CloudCrate.Application.Mappers;
 
 public static class CrateFolderMapper
 {
-    public static CrateFolderResponse ToCrtaCrateFolderResponse(CrateFolder folder)
+    public static CrateFolderResponse ToCrateFolderResponse(CrateFolder folder)
     {
-        return new CrateFolderResponse()
+        return new CrateFolderResponse
+        {
+            Id = folder.Id,
+            Name = folder.Name,
+            Color = folder.Color ?? "#EAAC00",
+            ParentFolderId = folder.ParentFolderId,
+            ParentFolderName = folder.ParentFolder?.Name,
+            CrateId = folder.CrateId,
+            CreatedAt = folder.CreatedAt,
+            UpdatedAt = folder.UpdatedAt,
+            IsDeleted = folder.IsDeleted
+        };
+    }
+
+    public static FolderResponse ToFolderResponse(CrateFolder folder)
+    {
+        return new FolderResponse
         {
             Id = folder.Id,
             Name = folder.Name,
             CrateId = folder.CrateId,
             ParentFolderId = folder.ParentFolderId,
             Color = folder.Color,
+            UploadedByUserId = folder.CreatedByUserId,
+            UploadedByDisplayName = folder.CreatedByUser?.DisplayName ?? "Unknown",
+            UploadedByEmail = folder.CreatedByUser?.Email ?? "",
+            UploadedByProfilePictureUrl = folder.CreatedByUser?.ProfilePictureUrl ?? "",
             CreatedAt = folder.CreatedAt
         };
     }
