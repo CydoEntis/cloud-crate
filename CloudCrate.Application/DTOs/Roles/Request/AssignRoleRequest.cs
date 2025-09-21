8dcc1c82a160e999c115633b8a051ff169c41c3c
Cody <cydoentis@gmail.com>
2025-09-21 12:22:07 -0400

    refactor: Removed UserId from AssignRoleRequest.cs since its coming in as a query param.

--- CloudCrate.Application/DTOs/Roles/Request/AssignRoleRequest.cs
+++ CloudCrate.Application/DTOs/Roles/Request/AssignRoleRequest.cs
@@ -1,9 +1,6 @@
-﻿using CloudCrate.Domain.Enums;
-
-namespace CloudCrate.Application.DTOs.Roles.Request;
+﻿namespace CloudCrate.Application.DTOs.Roles.Request;
 
 public class AssignRoleRequest
 {
-    public string UserId { get; set; } = null!;
     public CrateRole Role { get; set; }
 }
